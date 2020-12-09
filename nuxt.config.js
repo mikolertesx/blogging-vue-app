7ca5c5bc0a401e766f92e983768892574088d3ff
Mikolertesx <mgangsal@outlook.com>
2020-12-09 15:27:36 -0600

    Starting the project

---
+++ nuxt.config.js
@@ -0,0 +1,18 @@
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: "MyBlog",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  css: [],
+  plugins: [],
+  components: true,
+  buildModules: [],
+  modules: [],
+  build: {}
+};
