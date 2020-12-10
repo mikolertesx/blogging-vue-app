79b8fd338f2f861798baeebb9e0063aff939a0be
Mikolertesx <mgangsal@outlook.com>
2020-12-10 14:34:36 -0600

    Preparing the backend

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="featured-posts">
     <PostPreview
       id="1"
+      :isAdmin="isAdmin"
       thumbnail="https://steamuserimages-a.akamaihd.net/ugc/913550211586682151/383DE3EAB05DAEA383A8D818965E8E2F8B33E658/?imw=1024&imh=631&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
       title="Hello there!"
       previewText="This is my first post!"
     />
     <PostPreview
       id="2"
+      :isAdmin="isAdmin"
       thumbnail="https://i.pinimg.com/originals/bf/0a/43/bf0a430202cf18bb37d256ed8fd28707.jpg"
       title="Hello there!"
       previewText="This is my first post!"
     />
     <PostPreview
       id="3"
+      :isAdmin="isAdmin"
       thumbnail="https://pm1.narvii.com/6980/277cb138cc5d3541a97aa7be0260d1e3aff37c0ar1-768-768v2_hq.jpg"
       title="Hello there!"
       previewText="This is my first post!"
@@ -21,10 +24,19 @@
   </section>
 </template>
 <script>
-import PostPreview from "@/components/POsts/PostPreview";
+import PostPreview from "@/components/Posts/PostPreview";
 export default {
-  PostPreview
-}
+  components: {
+    PostPreview,
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
 </script>
 <style scoped>
 .featured-posts {
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm";
+export default {
+  components: {
+    AdminPostForm,
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: "Maximilian",
+        title: "My awesome post",
+        content: "Super amazing, thanks for that!",
+        thumbnailLink:
+          "https://steamuserimages-a.akamaihd.net/ugc/913550211586682151/383DE3EAB05DAEA383A8D818965E8E2F8B33E658/?imw=1024&imh=631&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true",
+      },
+    };
+  },
+};
+</script>
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
