b6372837bad4faf85db628ef214fb51fb1584f3c
Mike <mgangsal@outlook.com>
2020-12-25 14:21:57 -0600

    Quick bugfix

--- store/index.js
+++ store/index.js
@@ -143,8 +143,10 @@ export const actions = {
     vuexContext.commit("clearToken");
     Cookie.remove("jwt");
     Cookie.remove("expirationDate");
-    localStorage.removeItem("token");
-    localStorage.removeItem("tokenExpiration");
+    if (process.client) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("tokenExpiration");
+    }
   }
 };
 
